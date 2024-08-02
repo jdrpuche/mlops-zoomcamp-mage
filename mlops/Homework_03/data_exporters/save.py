461b6d0023089ef2b8418cff124c2aa91f69d764
jdrpuche <jdrpuche@gmail.com>
2024-08-02 21:14:06 +0000

    small changes

--- mlops/Homework_03/data_exporters/save.py
+++ mlops/Homework_03/data_exporters/save.py
@@ -1,8 +1,8 @@
 import pickle
 import mlflow
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment(EXPERIMENT_NAME)
+mlflow.set_tracking_uri("http://mlflow:5000")
+mlflow.set_experiment('nyc-taxi-experiment')
 
 
 if 'data_exporter' not in globals():
